7817a871c4003821e1738cbd09dc1388eda3e3e6
KunderAishwarya <aishwarya.kunder26@gmail.com>
2019-11-02 04:56:09 -0700

    dffgrgv

---
+++ TestProject/src/Customer.java
@@ -0,0 +1,20 @@
+
+public class Customer {
+	int custID;
+	String custName;
+	public int getCustID() {
+		return custID;
+	}
+	public void setCustID(int custID) {
+		this.custID = custID;
+	}
+	public String getCustName() {
+		return custName;
+	}
+	public void setCustName(String custName) {
+		this.custName = custName;
+	}
+	
+	
+	
+}
